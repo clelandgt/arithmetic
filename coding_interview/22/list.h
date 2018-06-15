0d4ce7b1a4174cded5645d37e4e825954321f554
cleland <clelandgt@163.com>
2018-06-15 08:22:50 +0800

    剑指offer添加新增22题

---
+++ coding_interview/22/list.h
@@ -0,0 +1,22 @@
+//
+// Created by cleland on 2018/6/8.
+//
+
+#ifndef INC_21_LIST_H
+#define INC_21_LIST_H
+
+#endif //INC_21_LIST_H
+
+struct ListNode
+{
+    int       m_nValue;
+    ListNode* m_pNext;
+};
+
+ListNode* CreateListNode(int value);
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext);
+void PrintListNode(ListNode* pNode);
+void PrintList(ListNode* pHead);
+void DestroyList(ListNode* pHead);
+void AddToTail(ListNode** pHead, int value);
+void RemoveNode(ListNode** pHead, int value);
