2c5eb8f2c319ccca3b0ce793ddb7854f1b74f5d0
cleland <clelandgt@163.com>
2019-10-21 23:01:54 +0800

    new bmC

---
+++ basic/python/string_matching/bm.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @File  : bm.py
+# @Author: clelandgt@163.com
+# @Date  : 2019-10-17
+# @Desc  :
+
+
+def bm():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
