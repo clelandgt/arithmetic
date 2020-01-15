c1df179f980c2ca28d31396c64a86e36ef2e5648
gantao <clelandgt@163.com>
2020-01-15 19:12:48 +0800

    ADD: binary_tree

---
+++ basic/python/data_structure/tree/binary_tree.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : binary_tree.py
+# @Author: clelandgt@163.com
+# @Date  : 2019-07-25
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
