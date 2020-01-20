14632a0f04fd37f3dd2d17e1020939f023fc9a71
cleland <clelandgt@163.com>
2020-01-20 08:13:37 +0800

    UPD: leetcode 1

--- leetcode/1/two_sum.py
+++ leetcode/1/two_sum.py
@@ -9,7 +9,7 @@ from typing import List
 class Solution1:
     """ 两次遍历，暴力求解 """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if len(nums)<=1:
+        if len(nums) <= 1:
             return False
 
         for i in range(len(nums)):
@@ -18,11 +18,26 @@ class Solution1:
                     return i, j
 
 
+class Solution2:
+    """ 一个哈希表+单次数组遍历
+    哈希表的对应的key=target-item(遍历nums的元素), value=item对应的下标
+    """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) <= 1:
+            False
+
+        tmp_dict = {}
+        for i in range(len(nums)):
+            if (target-nums[i]) in tmp_dict.keys():
+                return tmp_dict[target-nums[i]], i
+            else:
+                tmp_dict[nums[i]] = i
+
 
 def main():
     nums = [2, 7, 11, 15]
     target = 9
-    s = Solution1()
+    s = Solution2()
     print(s.twoSum(nums, target))
 
 
