7dae18b0f7973a5e2fe0581b0d1b5fa99964afdf
gantao <clelandgt@163.com>
2020-01-28 23:29:40 +0800

    ADD leetcode 204

---
+++ leetcode/204/count_primes.py
@@ -0,0 +1,13 @@
+git # -*- coding: utf-8 -*-
+# @File  : count_primes.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-01-28
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
