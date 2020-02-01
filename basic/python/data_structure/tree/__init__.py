9eb0002414bdc342219371ffb4877a50fe0bf5d2
gantao <clelandgt@163.com>
2020-02-01 22:06:43 +0800

    ADD: binary breee

--- basic/python/data_structure/tree/__init__.py
+++ basic/python/data_structure/tree/__init__.py
@@ -3,6 +3,7 @@
 # @Author: Cleland
 # @Date  : 2019-03-01
 # @Desc  :
+from abc import abstractmethod
 
 
 class Node(object):
@@ -10,3 +11,69 @@ class Node(object):
         self.left = None
         self.right = None
         self.value = value
+
+
+class BinaryTree(object):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def insert(self, value):
+        """ 插入
+        :param value:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def find(self, value):
+        """ 查找
+        :param value:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def delete(self, value):
+        """ 删除
+        :param value:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def get_max(self, value):
+        """ 获取最大值
+        :param value:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def get_min(self, value):
+        """ 获取最小值
+        :param value:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def pre_order(self):
+        """ 前序遍历
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def in_order(self):
+        """ 中序遍历
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def post_order(self):
+        """ 后序遍历
+        :return:
+        """
+        pass
