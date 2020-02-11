f7b7b4da93f1298c9f9d5ad07822f00f7d78896d
gantao <clelandgt@163.com>
2020-02-11 22:54:08 +0800

    UPD: get_max_permutation

--- basic/python/recursion/get_max_permutation.py
+++ basic/python/recursion/get_max_permutation.py
@@ -3,20 +3,22 @@
 # @Author: clelandgt@163.com
 # @Date  : 2020-02-11
 # @Desc  : [Python算法教程][第4章 归纳、递归与规归简][基于归纳法(与递归法)的设计][4.4.1寻找最大排列]
-#          解题思路图片连接：
+#          解题思路图片连接：1. https://cleland.oss-cn-beijing.aliyuncs.com/arithmetic/python%E7%AE%97%E6%B3%95%E6%95%99%E7%A8%8B/4-5/4.4.1_1.jpeg
+#                          2. https://cleland.oss-cn-beijing.aliyuncs.com/arithmetic/python%E7%AE%97%E6%B3%95%E6%95%99%E7%A8%8B/4-5/4.4.1_1.jpeg
 
 
 def get_max_permutation1(nums, remain_index=[]):
     if not remain_index:
         remain_index = list(range(len(nums)))
-
-    diff = set(remain_index) - set([nums[item] for item in remain_index])
+    if len(remain_index) == 1:
+        return remain_index
+    remain_value = [nums[item] for item in remain_index]
+    diff = set(remain_index) - set(remain_value)
     if len(diff) == 0:
-        return
+        return remain_index
     else:
         remain_index.remove(diff.pop())
-        get_max_permutation1(nums, remain_index)
-        return set(nums)
+        return get_max_permutation1(nums, remain_index)
 
 
 def main():
