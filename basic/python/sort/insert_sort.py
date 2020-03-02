50c0f600f351386dd63a25dc78ee1232d3f77451
gantao <clelandgt@163.com>
2020-03-02 23:36:44 +0800

    UPD: insert_sort

--- basic/python/sort/insert_sort.py
+++ basic/python/sort/insert_sort.py
@@ -1,45 +1,44 @@
-# -*- coding:utf-8 -*-
-__author__ = 'cleland'
-""" 插入排序
+# -*- coding: utf-8 -*-
+# @File  : insert_sort.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-03-02
+# @Desc  : 插入排序
+"""
 已排序的[0, n-1]加入n后再排序。
 """
 
 
-def insert_sort1(nums):
-    """循环的方式实现"""
-    for i in range(1, len(nums)):
+def insert_sort(l):
+    """基于循环"""
+    for i in range(len(l)):
         for j in range(1, i+1)[::-1]:
-            if nums[j] < nums[j-1]:
-                nums[j], nums[j-1] = nums[j-1], nums[j]
-
-    return nums
-
-
-def insert_sort2(nums):
-    """递归的方式实现"""
-    return _insert_sort2(nums, len(nums))
+            if l[j] < l[j-1]:
+                l[j], l[j-1] = l[j-1], l[j]
+    return l
 
 
-def _insert_sort2(nums, n):
+def insert_sort2(l):
+    """基于递归"""
+    return _insert_sort2(l, len(l))
+
+
+def _insert_sort2(l, n):
     if n == 0:
         return
-    # 对于n-1进行排序
-    _insert_sort2(nums, n-1)
-    # 将n加入已排序的n-1序列里
+    _insert_sort2(l, n-1)
 
     for i in range(1, n)[::-1]:
-        if nums[i] < nums[i-1]:
-            nums[i], nums[i-1] = nums[i-1], nums[i]
-            break
+        if l[i] < l[i-1]:
+            l[i], l[i-1] = l[i-1], l[i]
 
-    return nums
+    return l
 
 
 def main():
     l1 = [5, 2, 4, 6, 10, 1, 3, 1, 23, 2, 9]
-    print ('before sort:', l1)
+    print('before sort:', l1)
     l2 = insert_sort2(l1)
-    print ('after sort:', l2)
+    print('after sort:', l2)
 
 
 if __name__ == '__main__':
