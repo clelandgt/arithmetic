cefb8ef563f7099efbb0b90cb0c2fb3d2fb7cf7a
gantao <clelandgt@163.com>
2020-03-02 23:41:19 +0800

    UPD: heap_sort

--- basic/python/sort/heap_sort.py
+++ basic/python/sort/heap_sort.py
@@ -1,3 +1,13 @@
-# -*- coding:utf-8 -*-
-""" 堆排序
-"""
+# -*- coding: utf-8 -*-
+# @File  : heap_sort.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-03-02
+# @Desc  : 堆排序
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
