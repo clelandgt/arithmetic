2234eccf190cd92097444504113042ba7e7fefd2
gantao <clelandgt@163.com>
2020-03-03 23:08:02 +0800

    UPD: bucket_sort

--- basic/python/sort/bucket_sort.py
+++ basic/python/sort/bucket_sort.py
@@ -11,12 +11,14 @@ def bucket_sort(nums, bucket_size=BUCKET_SIZE):
     max_value = max(nums)
     min_value = min(nums)
 
+    # 分桶
     bucket_count = ((max_value - min_value) // bucket_size) + 1
     buckets = [[] for _ in range(bucket_count)]
 
     for i in range(len(nums)):
         buckets[int(nums[i] - min_value) // bucket_size].append(nums[i])
 
+    # 桶内排序
     return sorted(
         [buckets[i][j] for i in range(len(buckets)) for j in range(len(buckets[i]))]
     )
