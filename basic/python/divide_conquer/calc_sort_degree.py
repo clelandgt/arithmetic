92520bd3ddc08609f7e64c838bbf0c87d387aad7
gantao <clelandgt@163.com>
2020-03-10 23:57:12 +0800

    UPD: calc_sort_degree

--- basic/python/divide_conquer/calc_sort_degree.py
+++ basic/python/divide_conquer/calc_sort_degree.py
@@ -2,6 +2,23 @@
 # @File  : calc_sort_degree.py
 # @Author: clelandgt@163.com
 # @Date  : 2019-10-30
-# @Desc  :
+# @Desc  : https://time.geekbang.org/column/article/73503
+"""
+问题：假设我们有 n 个数据，我们期望数据从小到大排列，那完全有序的数据的有序度就是 n(n-1)/2，逆序度等于 0；
+     相反，倒序排列的数据的有序度就是 0，逆序度是 n(n-1)/2。除了这两种极端情况外，我们通过计算有序对或者
+     逆序对的个数，来表示数据的有序度或逆序度。
+eg:  2,4,3,1,5,6 逆序个数为4：(2,1) (4,3) (4,1) (3,1)
+"""
 
 
+def calc_sort_degree1(nums):
+    """ 暴力求解直接遍历 """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
