7463e06660f778d26d81a5733064da3e0b02508b
cleland <clelandgt@163.com>
2020-06-23 23:11:53 +0800

    UPD: min_number [greedy]

---
+++ basic/python/greedy/min_number.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# @File  : min_number.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-06-23
+# @Desc  :
+
+""" 问题描述
+在一个非负数整数a中，我们希望从中移除k个数字，让剩下的数字值最小，如何选择移除那k个数字
+"""
+
+
+def get_min_number(number, k):
+    if len(str(number)) < k:
+        return 0
+
+    nums = list(str(number))
+    index = 0
+    while k > 0:
+        if nums[index] > nums[index+1]:
+            del nums[index]
+            index -= 1
+            k -= 1
+        else:
+            index += 1
+
+    return int(''.join(nums))
+
+
+def main():
+    number = 3455473145238
+    k = 5
+    print('length: ', len(str(number)))
+
+    result = get_min_number(number, k)
+    print('get_min_number1 result: ', result)
+    print('length: ', len(str(result)))
+
+
+if __name__ == '__main__':
+    main()
