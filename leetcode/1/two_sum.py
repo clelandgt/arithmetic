23cecb0f05ea1bb786a2fc785c5ede0c5bce52cd
cleland <clelandgt@163.com>
2020-06-26 00:16:12 +0800

    UPD: leetcode 1

--- leetcode/1/two_sum.py
+++ leetcode/1/two_sum.py
@@ -7,7 +7,9 @@ from typing import List
 
 
 class Solution1:
-    """ 两次遍历，暴力求解 """
+    """ 两层遍历，暴力求解(Brute Force)
+     Time: O(n^2)  Space: O(1)
+    """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         if len(nums) <= 1:
             return False
@@ -21,7 +23,22 @@ class Solution1:
 class Solution2:
     """ 一个哈希表+单次数组遍历
     哈希表的对应的key=target-item(遍历nums的元素), value=item对应的下标
+    Time: O(n)  Space: O(n)
     """
+
+    def _twoSum_1(self, nums: List[int], target: int) -> List[int]:
+        """version1
+        改进点：1. len(nums)<=1 未做处理；2. 不需单独遍历一次生成哈希表
+        """
+        nums_dict = {}
+        for i, item in enumerate(nums):
+            nums_dict[item] = i
+
+        for i, item in enumerate(nums):
+            j = target - item
+            if nums_dict.get(j) is not None and i != nums_dict.get(j):
+                return i, nums_dict[j]
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         if len(nums) <= 1:
             False
