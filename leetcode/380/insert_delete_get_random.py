938f3cfcaa81ddafd9f5c95a6d29ebfc1e1296ff
cleland <clelandgt@163.com>
2020-06-27 19:52:58 +0800

    ADD: 380

---
+++ leetcode/380/insert_delete_get_random.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @File  : insert_delete_get_random.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-06-27
+# @Desc  :
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
