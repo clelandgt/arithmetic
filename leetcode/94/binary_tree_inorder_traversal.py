6341b0dbe969c3d12ebf4a9850759fb0f40be3e8
cleland <clelandgt@163.com>
2020-06-27 11:24:10 +0800

    ADD: 94

---
+++ leetcode/94/binary_tree_inorder_traversal.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# @File  : binary_tree_inorder_traversal.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-06-27
+# @Desc  :
+
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution1:
+    _in_nums = []
+
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        self._in_nums = []
+        self._inorderTraversal(root)
+        return self._in_nums
+
+    def _inorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return
+        self._inorderTraversal(root.left)
+        self._in_nums.append(root.val)
+        self._inorderTraversal(root.right)
+
+
+def main():
+    test_cases = [
+        TreeNode(1, None, TreeNode(2, TreeNode(3))),
+        None,
+    ]
+
+    print('Solution1')
+    s = Solution1()
+    for test_case in test_cases:
+        print(s.inorderTraversal(test_case))
+
+
+if __name__ == '__main__':
+    main()
