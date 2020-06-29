002db423beffcae713b600eddfcf255d22a58f20
cleland <clelandgt@163.com>
2020-06-29 23:32:36 +0800

    ADD: 160

---
+++ leetcode/160/intersection_of_two_linked_lists.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @File  : intersection_of_two_linked_lists.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-06-29
+# @Desc  :
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
