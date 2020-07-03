21d5dbfe51dc8b586028fcf7326253547b059aac
cleland <clelandgt@163.com>
2020-07-03 23:23:40 +0800

    UPD: leetcode 283

--- leetcode/283/move_zeroes.py
+++ leetcode/283/move_zeroes.py
@@ -11,6 +11,15 @@ class Solution1:
         nums.sort(key=lambda x: 1 if x == 0 else 0)
 
 
+class Solution2:
+    def moveZeroes(self, nums: List[int]) -> None:
+        not_null_index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[not_null_index], nums[i] = nums[i], nums[not_null_index]
+                not_null_index += 1
+
+
 def main():
     test_cases = [
         [0, 1, 0, 3, 12]
@@ -20,6 +29,12 @@ def main():
     s1 = Solution1()
     for test_case in test_cases:
         s1.moveZeroes(test_case)
+        print(test_case)
+
+    print('Solution2')
+    s2 = Solution2()
+    for test_case in test_cases:
+        s2.moveZeroes(test_case)
         print(test_case)
 
 
