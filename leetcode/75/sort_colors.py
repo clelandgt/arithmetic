5dcc462858acad8cf0b9d1beb3f5ce9447ce639d
cleland <clelandgt@163.com>
2020-07-18 10:21:10 +0800

    ADD: 75

---
+++ leetcode/75/sort_colors.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @File  : sort_colors.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-07-18
+# @Desc  :
+from typing import List
+
+
+class Solution1:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
