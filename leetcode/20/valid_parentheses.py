85f0ae3717e783420fe6344a85aac49cc966f21f
cleland <clelandgt@163.com>
2020-07-22 21:49:45 +0800

    ADD: leetcode 20

---
+++ leetcode/20/valid_parentheses.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @File  : valid_parentheses.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-07-22
+# @Desc  :
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
