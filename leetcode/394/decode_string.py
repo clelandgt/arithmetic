ef887e7d9c9d95d27524a12a665891385b8b9c75
cleland <clelandgt@163.com>
2020-07-23 23:37:51 +0800

    ADD: leetcode 394

---
+++ leetcode/394/decode_string.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @File  : decode_string.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-07-23
+# @Desc  :
+
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
