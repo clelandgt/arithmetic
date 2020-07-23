22d5327009e46f79d21cf9688101322f230afde4
cleland <clelandgt@163.com>
2020-07-24 00:19:40 +0800

    UPD: leetcode 394

--- leetcode/394/decode_string.py
+++ leetcode/394/decode_string.py
@@ -5,13 +5,45 @@
 # @Desc  :
 
 
-class Solution:
+class Solution1:
     def decodeString(self, s: str) -> str:
-        pass
+        num = 0
+        string = ''
+        stack_str = []
+        stack_num = []
+
+        for item in s:
+            if item.isdigit():
+                num = num * 10 + int(item)
+            elif item == '[':
+                stack_str.append(string)
+                stack_num.append(num)
+                string = ''
+                num = 0
+            elif item.isalpha():
+                string += item
+                num = 0
+            elif item == ']':
+                pre_string = stack_str.pop()
+                pre_string += (string * stack_num.pop())
+                string = pre_string
+                num = 0
+
+        return string
 
 
 def main():
-    pass
+    test_cases = [
+        '3[a]2[bc]',
+        '3[a2[c]]',
+        '2[abc]3[cd]ef',
+        'abc13[cd]xyz'
+    ]
+
+    print('Solution1')
+    s1 = Solution1()
+    for test_case in test_cases:
+        print(s1.decodeString(test_case))
 
 
 if __name__ == '__main__':
