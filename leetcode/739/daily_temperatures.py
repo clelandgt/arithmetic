c4f9140896a2c79615f1e9022b82533e41f099e8
cleland <clelandgt@163.com>
2020-07-25 11:53:22 +0800

    UPD: leetcode 739

--- leetcode/739/daily_temperatures.py
+++ leetcode/739/daily_temperatures.py
@@ -27,6 +27,32 @@ class Solution1:
         return result
 
 
+class Solution2:
+    """暴力破解: 代码更加优雅"""
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        res = [0] * len(T)
+        for i in range(len(T)):
+            for j in range(i, len(T)):
+                if T[j] > T[i]:
+                    res[i] = j - i
+                    break #guarentee the soonest
+        return res
+
+
+class Solution3:
+    """使用栈"""
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        res = [0] * len(T)
+        stack = []
+        for i in range(len(T)-1, -1, -1):
+            while(stack and T[i] >= T[stack[-1]]):
+                stack.pop()
+            if stack:
+                res[i] = stack[-1] - i
+            stack.append(i)
+        return res
+
+
 def main():
     test_cases = [
         [73, 74, 75, 71, 69, 72, 76, 73]
@@ -37,6 +63,10 @@ def main():
     for test_case in test_cases:
         print(s1.dailyTemperatures(test_case))
 
+    print('Solution2')
+    s1 = Solution2()
+    for test_case in test_cases:
+        print(s1.dailyTemperatures(test_case))
 
 
 if __name__ == '__main__':
