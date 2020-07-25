6ca67311e12bc0679ed66de7f1d6ca38f3534868
cleland <clelandgt@163.com>
2020-07-25 23:34:26 +0800

    UPD: leetcode 138

--- leetcode/138/copy_list_with_random_pointer.py
+++ leetcode/138/copy_list_with_random_pointer.py
@@ -3,11 +3,39 @@
 # @Author: clelandgt@163.com
 # @Date  : 2020-07-25
 # @Desc  :
+# Definition for a Node.
 
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution1:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head:
+            return
+        mapping = {}
+        p = head
+        while p:
+            mapping[p] = Node(p.val, None, None)
+            p = p.next
+
+        p = head
+        while p:
+            if p.next:
+                mapping[p].next = mapping[p.next]
+            if p.random:
+                mapping[p].random = mapping[p.random]
+            p = p.next
+
+        return mapping[head]
 
 
 def main():
-    pass
+    print('Solution1')
 
 
 if __name__ == '__main__':
