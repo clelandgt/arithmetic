abb9c606b6751fb9f2620a2644cae3d3e4f62e84
cleland <clelandgt@163.com>
2020-07-26 10:10:08 +0800

    ADD: leetcode 34

---
+++ leetcode/34/find_first_and_last_position_of_element_in_sorted_array.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @File  : find_first_and_last_position_of_element_in_sorted_array.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-07-26
+# @Desc  :
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
