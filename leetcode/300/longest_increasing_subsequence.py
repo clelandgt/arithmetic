ca2d46ac36949ee4ece153c791dbd46cf8b2386f
cleland <clelandgt@163.com>
2020-08-01 18:31:49 +0800

    ADD: leetcode 300

---
+++ leetcode/300/longest_increasing_subsequence.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @File  : longest_increasing_subsequence.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-08-01
+# @Desc  :
+from typing import  List
+
+
+class Solution1:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
