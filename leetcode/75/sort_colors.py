6a54126c0567882b36aa6e34259f450f0449062d
cleland <clelandgt@163.com>
2020-08-01 19:03:44 +0800

    UPD: 75

--- leetcode/75/sort_colors.py
+++ leetcode/75/sort_colors.py
@@ -11,10 +11,28 @@ class Solution1:
         """
         Do not return anything, modify nums in-place instead.
         """
+        values_count = {
+            '0': 0,
+            '1': 0,
+            '2': 0
+        }
+        for num in nums:
+            values_count[str(num)] += 1
+
+        result = [0] * values_count['0'] + [1] * values_count['1'] + [2] * values_count['2']
+        for index, item in enumerate(nums):
+            nums[index] = result[index]
 
 
 def main():
-    pass
+    test_cases = [
+        [2, 0, 2, 1, 1, 0]
+    ]
+    print('Solution1')
+    s1 = Solution1()
+    for test_case in test_cases:
+        s1.sortColors(test_case)
+        print(test_case)
 
 
 if __name__ == '__main__':
