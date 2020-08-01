f44ae5c9eb24f5e1c3b661137596d9b5302c8d92
cleland <clelandgt@163.com>
2020-08-01 18:48:35 +0800

    ADD: leetcode 300

--- leetcode/300/longest_increasing_subsequence.py
+++ leetcode/300/longest_increasing_subsequence.py
@@ -12,7 +12,14 @@ class Solution1:
 
 
 def main():
-    pass
+    test_cases = [
+        [10, 9, 2, 5, 3, 7, 101, 18]
+    ]
+
+    print('Solution1')
+    s1 = Solution1()
+    for test_case in test_cases:
+        print(s1.lengthOfLIS(test_case))
 
 
 if __name__ == '__main__':
