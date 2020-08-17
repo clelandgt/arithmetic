afa4aa047ade936b3a68bb377de78e29a84f0368
cleland <clelandgt@163.com>
2020-08-17 22:43:01 +0800

    UPD: bubble_sort

--- basic/python/sort/bubble_sort.py
+++ basic/python/sort/bubble_sort.py
@@ -5,27 +5,22 @@
 # @Desc  : 冒泡
 
 
-def bubble_sort(l):
-    """ 冒泡排序
+def bubble_sort(nums):
+    """ 冒泡排序O(n^2)
     每次比较如果发现较小的元素在后面，就交换两个相邻的元素。排序序列顺序从左到右是从小到大，伪代码：
-    for i in [0, n)
-        for j in [n-1, i) #逆序
-            if a[j] < a[j-1]
-                swap(a[j], a[j-1])
     """
-    for i in range(len(l)):
-        for j in range(i+1, len(l))[::-1]:
-            if l[j-1] >= l[j]:
-                l[j-1], l[j] = l[j], l[j-1]
-
-    return l
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums))[::-1]:
+            if nums[j] < nums[j-1]:
+                nums[j], nums[j-1] = nums[j-1], nums[j]
+    return nums
 
 
 def main():
-    l1 = [5, 2, 4, 6, 10, 1, 3, 1, 23, 2, 9]
-    print('before sort:', l1)
-    l2 = bubble_sort(l1)
-    print('after sort:', l2)
+    nums1 = [5, 2, 4, 6, 10, 1, 3, 1, 23, 2, 9]
+    print('before sort:', nums1)
+    nums2 = bubble_sort(nums1)
+    print('after sort:', nums2)
 
 
 if __name__ == '__main__':
