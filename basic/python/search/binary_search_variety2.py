ab6ce84e956cda53e24b4399cdf26b48def29ddb
cleland <clelandgt@163.com>
2020-08-31 08:20:53 +0800

    UPD: binary_search_variety2

--- basic/python/search/binary_search_variety2.py
+++ basic/python/search/binary_search_variety2.py
@@ -5,8 +5,33 @@
 # @Desc  : 二分查找最后一个等于给定元素的值
 
 
+def binary_search1(nums, num):
+    print(nums)
+    left, right = 0, len(nums)-1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if num < nums[mid]:
+            right = mid - 1
+        elif num > nums[mid]:
+            left = mid + 1
+        else:
+            if mid == len(nums)-1 or num != nums[mid+1]:
+                return mid
+            else:
+                left = mid + 1
+
+
 def main():
-    pass
+    test_cases = [
+        [[1, 3, 4, 5, 6, 8, 8, 8, 11, 18], 8]
+    ]
+    for test_case in test_cases:
+        print('binary_search1')
+        index = binary_search1(test_case[0], test_case[1])
+        print('nums: ', test_case[0])
+        print('search: ', test_case[1])
+        print('index: ', index)
 
 
 if __name__ == '__main__':
