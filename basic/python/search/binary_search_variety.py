2335ec6a75eda345d40570ce39f2de64d83d9618
cleland <clelandgt@163.com>
2020-08-31 08:03:20 +0800

    ADD: binary_search_variety

---
+++ basic/python/search/binary_search_variety.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : binary_search_variety.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-08-31
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
