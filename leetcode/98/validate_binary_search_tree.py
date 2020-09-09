ab5c7a6c05802791049f57a0360b60f81061df58
cleland <clelandgt@163.com>
2020-09-09 23:46:19 +0800

    ADD: leetcode 98

---
+++ leetcode/98/validate_binary_search_tree.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : validate_binary_search_tree.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-09-09
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
