1a67c6c330a3db83323bf698e3bfc13ebdb02b75
cleland <clelandgt@163.com>
2020-09-09 23:54:08 +0800

    UPD: leetcode 98

--- leetcode/98/validate_binary_search_tree.py
+++ leetcode/98/validate_binary_search_tree.py
@@ -5,8 +5,34 @@
 # @Desc  :
 
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution1:
+    def isValidBST(self, root: TreeNode) -> bool:
+        pass
+
+
 def main():
-    pass
+    tree1 = TreeNode(2)
+    tree1.left = TreeNode(1)
+    tree1.right = TreeNode(3)
+
+    tree2 = TreeNode(5)
+    tree2.left = TreeNode(1)
+    tree2.right = TreeNode(4)
+    tree2.right.left = TreeNode(3)
+    tree2.right.right = TreeNode(6)
+
+    test_cases = [tree1, tree2]
+    s1 = Solution1()
+    for test_case in test_cases:
+        print(s1.isValidBST(test_case))
 
 
 if __name__ == '__main__':
