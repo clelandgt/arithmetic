fd1889714cc2114f2807c0b0c8ac9db70e8b4d79
cleland <clelandgt@163.com>
2020-09-11 08:38:53 +0800

    UPD: leetcode 105

--- leetcode/105/construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ leetcode/105/construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -16,7 +16,9 @@ class TreeNode:
 
 
 class Solution1:
-    """超时了"""
+    """超时了
+    突破点: pre前序的第一个元素是顶底；该元素将中序切分为 左子叶子树，右子叶子树。
+    """
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if inorder is None or len(inorder) == 0:
             return
@@ -32,11 +34,26 @@ class Solution1:
         return root
 
 
+class Solution2:
+    """
+    基于Solution1优化。
+    """
+    def buildTree(self, preorder, inorder):
+        if inorder is None or len(inorder) == 0:
+            return
+
+        ind = inorder.index(preorder.pop(0))
+        root = TreeNode(inorder[ind])
+        root.left = self.buildTree(preorder, inorder[0:ind])
+        root.right = self.buildTree(preorder, inorder[ind + 1:])
+        return root
+
+
 def main():
     test_cases = [
         [[3, 9, 20, 15, 7], [9, 3, 15, 20, 7]]
     ]
-    s1 = Solution1()
+    s1 = Solution2()
     for test_case in test_cases:
         tree1 = s1.buildTree(test_case[0], test_case[1])
         pass
