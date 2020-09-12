ecd32843927a32c0f266473d42c089e86045c838
cleland <clelandgt@163.com>
2020-09-12 18:49:36 +0800

    UPD: leetcode 226

--- leetcode/226/invert_binary_tree.py
+++ leetcode/226/invert_binary_tree.py
@@ -5,8 +5,39 @@
 # @Desc  :
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution1:
+    def __init__(self):
+        self.root = None
+
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if self.root is None:
+            self.root = root
+        if root is None:
+            return
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return self.root
+
+
 def main():
-    pass
+    tree1 = TreeNode(4)
+    tree1.left = TreeNode(2, TreeNode(1), TreeNode(3))
+    tree1.right = TreeNode(7, TreeNode(6), TreeNode(9))
+
+    test_cases = [
+        tree1
+    ]
+    s1 = Solution1()
+    for test_case in test_cases:
+        node = s1.invertTree(test_case)
 
 
 if __name__ == '__main__':
