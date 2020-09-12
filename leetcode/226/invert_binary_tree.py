9937b7c306f656d4798fa8825835a09c4c529804
cleland <clelandgt@163.com>
2020-09-12 18:39:02 +0800

    ADD: leetcode 226

---
+++ leetcode/226/invert_binary_tree.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : invert_binary_tree.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-09-12
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
