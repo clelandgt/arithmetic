1587d13b9dc80260c3664536b3d8121fc142e189
cleland <clelandgt@163.com>
2020-09-12 19:25:04 +0800

    ADD: 236

---
+++ leetcode/236/lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @File  : lowest_common_ancestor_of_a_binary_tree.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-09-12
+# @Desc  :
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
