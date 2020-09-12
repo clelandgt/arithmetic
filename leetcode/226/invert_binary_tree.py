b0efc0dfe47918b70ba83a50332f11ea12d2a22d
cleland <clelandgt@163.com>
2020-09-12 19:14:11 +0800

    UPD: leetcode 226

--- leetcode/226/invert_binary_tree.py
+++ leetcode/226/invert_binary_tree.py
@@ -3,6 +3,7 @@
 # @Author: clelandgt@163.com
 # @Date  : 2020-09-12
 # @Desc  :
+import copy
 
 
 class TreeNode:
@@ -27,17 +28,33 @@ class Solution1:
         return self.root
 
 
+class Solution2:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+
+
 def main():
     tree1 = TreeNode(4)
     tree1.left = TreeNode(2, TreeNode(1), TreeNode(3))
     tree1.right = TreeNode(7, TreeNode(6), TreeNode(9))
 
     test_cases = [
-        tree1
+        copy.deepcopy(tree1)
     ]
     s1 = Solution1()
     for test_case in test_cases:
         node = s1.invertTree(test_case)
+        pass
+
+    test_cases = [
+        copy.deepcopy(tree1)
+    ]
+    s2 = Solution2()
+    for test_case in test_cases:
+        node = s2.invertTree(test_case)
+        pass
 
 
 if __name__ == '__main__':
