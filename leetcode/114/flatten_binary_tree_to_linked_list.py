ebd24ba46b454316ef57855ec3608ae1e0ea63e7
cleland <clelandgt@163.com>
2020-09-12 10:31:26 +0800

    ADD: leetcode 114

---
+++ leetcode/114/flatten_binary_tree_to_linked_list.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : flatten_binary_tree_to_linked_list.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-09-12
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
