29d2978dd98b5b3732c19c7a411813dfea01ecaa
cleland <clelandgt@163.com>
2020-09-26 22:48:45 +0800

    ADD: dynamic_array

---
+++ basic/python/data_structure/array/dynamic_array.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @File  : dynamic_array.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-09-26
+# @Desc  : 实现一个支持动态扩容的数组, 并完成增删改差. 参考: https://blog.csdn.net/u013109501/article/details/88020739
+
+
+class DynamicArray(object):
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def get(self, index):
+        pass
+
+    def set(self, index, item):
+        pass
+
+    def remove(self, index):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
