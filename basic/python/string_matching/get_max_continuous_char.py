b5dda2e8ca77bb3e250e629ef11f879e2dc0594c
cleland <clelandgt@163.com>
2020-09-26 20:09:17 +0800

    ADD: get_max_continuous_char

---
+++ basic/python/string_matching/get_max_continuous_char.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @File  : get_max_continuous_char.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-09-26
+# @Desc  : 字符串里连续出现次数最多的字符
+
+
+def get_max_continuous_char(nums):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
