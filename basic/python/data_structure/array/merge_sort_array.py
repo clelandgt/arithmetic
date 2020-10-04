09c135298078f218811f8a59dbbcb5aae47a6388
gantao <clelandgt@163.com>
2020-10-04 20:43:09 +0800

    UPD: merge sort array

--- basic/python/data_structure/array/merge_sort_array.py
+++ basic/python/data_structure/array/merge_sort_array.py
@@ -2,16 +2,31 @@
 # @File  : merge_sort_array.py
 # @Author: tao.gan@advance.ai
 # @Date  : 2020-10-04
-# @Desc  :
+# @Desc  : https://blog.csdn.net/weixin_43633501/article/details/90110565
 
 
-class MergeSortArray(object):
-    def __init__(self):
-        pass
+def merge_sort_array(nums1, nums2):
+    results = []
+    while len(nums1) > 0 and len(nums2) > 0:
+        if nums1[0] <= nums2[0]:
+            results.append(nums1[0])
+            del nums1[0]
+        else:
+            results.append(nums2[0])
+            del nums2[0]
+    results.extend(nums1)
+    results.extend(nums2)
+    return results
+
+
+def print_array(nums):
+    print(nums)
 
 
 def main():
-    pass
+    num1 = [2, 6, 9, 12, 20]
+    num2 = [1, 3, 11, 12, 18]
+    print(merge_sort_array(num1, num2))
 
 
 if __name__ == '__main__':
