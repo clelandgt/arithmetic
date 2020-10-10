b30c0df4806bba3b37b2169a002b57240e1863c9
cleland <clelandgt@163.com>
2020-10-11 00:48:27 +0800

    ADD: leetcode 23

---
+++ leetcode/23/merge_list.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : merge_list.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-10-11
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
