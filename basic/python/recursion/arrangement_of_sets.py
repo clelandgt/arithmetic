190077a7306090993d4614120c14ddf65e403aca
cleland <clelandgt@163.com>
2020-10-11 17:14:59 +0800

    ADD: arrangement_of_sets

---
+++ basic/python/recursion/arrangement_of_sets.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @File  : arrangement_of_sets.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-10-11
+# @Desc  : 实现数据集合的全排列(TODO: 待实现)
+
+
+def arrangement_of_sets():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
