46e1b7a27937130fdfc27d1921f0fc5168409573
cleland <clelandgt@163.com>
2020-10-12 23:19:39 +0800

    ADD: linked hash table

---
+++ basic/python/data_structure/hash_table/linked_hash_table.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @File  : linked_hash_table.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-10-12
+# @Desc  :
+
+
+class LinkedHashTable:
+    def __init__(self):
+        pass
