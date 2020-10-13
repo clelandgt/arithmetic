c9185535797a36e17621d1acef55a072f9a9d7c1
cleland <clelandgt@163.com>
2020-10-14 07:57:38 +0800

    UPD: leetcode 56

--- leetcode/56/merge_intervals.py
+++ leetcode/56/merge_intervals.py
@@ -13,8 +13,8 @@ class Solution1:
         intervals = sorted(intervals, key=lambda x: x[0])
         result = [intervals[0]]
         for interval in intervals[1:]:
-            if result[-1][-1] >= interval[0]:
-                result[-1][-1] = interval[1] if result[-1][-1] < interval[1] else result[-1][-1]
+            if result[-1][1] >= interval[0]:
+                result[-1][1] = interval[1] if result[-1][1] < interval[1] else result[-1][1]
             else:
                 result.append(interval)
 
