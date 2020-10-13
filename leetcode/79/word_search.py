3aa820d358f8a8a7e2e75a7715da9aad796d107f
cleland <clelandgt@163.com>
2020-10-13 22:48:01 +0800

    UPD: leetcode 79

--- leetcode/79/word_search.py
+++ leetcode/79/word_search.py
@@ -8,11 +8,40 @@ from typing import List
 
 class Solution1:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        pass
+        if board is None or len(board) == 0:
+            return False
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0]:
+                    pass
+    def _search(self, board, word):
 
 
 def main():
-    pass
+    test_cases = [
+        ([
+            ['A', 'B', 'C', 'E'],
+            ['S', 'F', 'C', 'S'],
+            ['A', 'D', 'E', 'E']
+        ], 'ABCCED'),
+        ([
+             ['A', 'B', 'C', 'E'],
+             ['S', 'F', 'C', 'S'],
+             ['A', 'D', 'E', 'E']
+         ], 'SEE'),
+        ([
+             ['A', 'B', 'C', 'E'],
+             ['S', 'F', 'C', 'S'],
+             ['A', 'D', 'E', 'E']
+         ], 'ABCB')
+    ]
+
+    print('Solution1')
+    s1 = Solution1()
+    for test_case in test_cases:
+        s1.exist(test_case)
+
 
 
 if __name__ == '__main__':
