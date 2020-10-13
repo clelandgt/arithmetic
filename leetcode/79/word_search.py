53ba6c6f9265fe2ba469c4ccf9a54582cfc4374a
cleland <clelandgt@163.com>
2020-10-13 22:34:51 +0800

    ADD: leetcode 79

---
+++ leetcode/79/word_search.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @File  : word_search.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-10-13
+# @Desc  : https://leetcode-cn.com/problems/word-search/
+from typing import List
+
+
+class Solution1:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
