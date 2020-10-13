cc4a50cf1157b7d8455e4d36d3d23e7d785077c7
cleland <clelandgt@163.com>
2020-10-13 23:32:45 +0800

    UPD: leetcode 79

--- leetcode/79/word_search.py
+++ leetcode/79/word_search.py
@@ -14,9 +14,33 @@ class Solution1:
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if board[row][col] == word[0]:
-                    pass
-    def _search(self, board, word):
+                    if self._search(board, word[1:], [[row, col]]):
+                        return True
+        return False
 
+    def _search(self, board, word, indexs):
+        if len(word) == 0:
+            return True
+        width, high = len(board[0]), len(board)
+        current_char = word[0]
+        pre_index = indexs[-1]
+        if pre_index[1]-1 >= 0:
+            if board[pre_index[0]][pre_index[1]-1] == current_char and [pre_index[0], pre_index[1]-1] not in indexs:
+                if self._search(board, word[1:], indexs+[[pre_index[0], pre_index[1]-1]]):
+                    return True
+        if pre_index[1]+1 < width:
+            if board[pre_index[0]][pre_index[1]+1] == current_char and [pre_index[0], pre_index[1]+1] not in indexs:
+                if self._search(board, word[1:], indexs+[[pre_index[0], pre_index[1]+1]]):
+                    return True
+        if pre_index[0]-1 >= 0:
+            if board[pre_index[0]-1][pre_index[1]] == current_char and [pre_index[0]-1, pre_index[1]] not in indexs:
+                if self._search(board, word[1:], indexs+[[pre_index[0]-1, pre_index[1]]]):
+                    return True
+        if pre_index[0]+1 < high:
+            if board[pre_index[0]+1][pre_index[1]] == current_char and [pre_index[0]+1, pre_index[1]] not in indexs:
+                if self._search(board, word[1:], indexs+[[pre_index[0]+1, pre_index[1]]]):
+                    return True
+        return False
 
 def main():
     test_cases = [
@@ -40,8 +64,7 @@ def main():
     print('Solution1')
     s1 = Solution1()
     for test_case in test_cases:
-        s1.exist(test_case)
-
+        print(s1.exist(test_case[0], test_case[1]))
 
 
 if __name__ == '__main__':
