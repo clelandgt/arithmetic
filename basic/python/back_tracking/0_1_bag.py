4ea474bdfd8f580b2f8b134b50fc57f9e95d61c7
cleland <clelandgt@163.com>
2020-10-15 22:43:53 +0800

    ADD: 0_1_bag

---
+++ basic/python/back_tracking/0_1_bag.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @File  : 0_1_bag.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-10-15
+# @Desc  :
+
+
+def bag():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
