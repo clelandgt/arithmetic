fd339c62b97be0be40628bb8be9801c13d7c7a01
cleland <clelandgt@163.com>
2020-10-16 23:38:53 +0800

    UPD: 0_1_bag

--- basic/python/back_tracking/0_1_bag.py
+++ basic/python/back_tracking/0_1_bag.py
@@ -5,12 +5,16 @@
 # @Desc  :
 
 
-def bag():
+def bag(nums, bag_size):
     pass
 
 
 def main():
-    pass
+    test_cases = [
+        ([3, 1, 2, 5, 1, 3, 4, 8], 7)
+    ]
+    for test_case in test_cases:
+        print(bag(test_case[0], test_case[1]))
 
 
 if __name__ == '__main__':
