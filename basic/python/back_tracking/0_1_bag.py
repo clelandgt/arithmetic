94814e7ae86b9a168eedc5c6e221df794986b5d1
cleland <clelandgt@163.com>
2020-10-17 00:48:18 +0800

    UPD: 0_1_bag

--- basic/python/back_tracking/0_1_bag.py
+++ basic/python/back_tracking/0_1_bag.py
@@ -4,9 +4,27 @@
 # @Date  : 2020-10-15
 # @Desc  :
 
+max_size = float('-inf')
+
+
+def _bag(i, nums, current_size, bag_size):
+    global max_size
+
+    if current_size == bag_size or i == len(nums):
+        if current_size > max_size:
+            max_size = current_size
+        return
+
+    # 当前物品不放入背包
+    _bag(i+1, nums, current_size, bag_size)
+    # 当前物品放入背包
+    if current_size + nums[i] <= bag_size:
+        _bag(i+1, nums, current_size+nums[i], bag_size)
+
 
 def bag(nums, bag_size):
-    pass
+    _bag(0, nums, 0, bag_size)
+    return max_size
 
 
 def main():
