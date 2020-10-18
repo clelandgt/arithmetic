d5d0ebe03e39fc5594cbb10dbd017c9dfa7ca4b8
cleland <clelandgt@163.com>
2020-10-18 16:44:57 +0800

    UPD: pattrn

--- basic/python/back_tracking/pattern.py
+++ basic/python/back_tracking/pattern.py
@@ -18,7 +18,6 @@ def match(text, pattern):
 def rmatch(text, pattern, ti, pj):
     global match
     t_len, p_len = len(text), len(pattern)
-    if match: return
     if pj == p_len:
         if ti == t_len:
             match = True
@@ -26,17 +25,18 @@ def rmatch(text, pattern, ti, pj):
 
     if pattern[pj] == '*':
         for item in range(ti, t_len):
-            rmatch(text, pattern, item, pj)
-    elif pattern[pj] == 'j':
-        rmatch(text, pattern, ti, pj)
-        rmatch(text, pattern, ti+1, pj)
+            rmatch(text, pattern, item, pj+1)
+    elif pattern[pj] == '?':
+        rmatch(text, pattern, ti, pj+1)
+        rmatch(text, pattern, ti+1, pj+1)
     elif pattern[pj] == text[ti] and ti <t_len:
         rmatch(text, pattern, ti+1, pj+1)
 
 
 def main():
     test_cases = [
-        ('123abc4', '*bc?'),
+        ['123abc4', '*bc4'],
+        ['123abc4', '1*4']
     ]
 
     for test_case in test_cases:
