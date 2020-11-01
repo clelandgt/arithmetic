984bee6d165ed484e944cc7401c14e154cac92c4
cleland <clelandgt@163.com>
2020-11-01 18:40:34 +0800

    UPD: leetcode 75

--- leetcode/75/sort_colors.py
+++ leetcode/75/sort_colors.py
@@ -7,6 +7,10 @@ from typing import List
 
 
 class Solution1:
+    """计数排序
+    时间复杂度: O(n)
+    空间复杂度: O(n)
+    """
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -24,6 +28,34 @@ class Solution1:
             nums[index] = result[index]
 
 
+class Solution2:
+    """单指针
+    1. 迭代遍历, 把0放到数组之首；
+    2. 迭代遍历, 把1放到0之后；
+    时间复杂度: O(n)
+    空间复杂度: O(1)
+    """
+    def sortColors(self, nums: List[int]) -> None:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                nums[i], nums[count] = nums[count], nums[i]
+                count += 1
+
+        for i in range(count, len(nums)):
+            if nums[i] == 1:
+                nums[i], nums[count] = nums[count], nums[i]
+                count += 1
+
+        return nums
+
+
+class Solution3:
+    """双指针"""
+    def sortColors(self, nums: List[int]) -> None:
+        pass
+
+
 def main():
     test_cases = [
         [2, 0, 2, 1, 1, 0]
@@ -34,6 +66,11 @@ def main():
         s1.sortColors(test_case)
         print(test_case)
 
+    print('Solution2')
+    s2 = Solution2()
+    for test_case in test_cases:
+        s2.sortColors(test_case)
+        print(test_case)
 
 if __name__ == '__main__':
     main()
