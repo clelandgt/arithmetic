b3c091919ce2449f671b88e96b7b695c10365a8b
cleland <clelandgt@163.com>
2020-11-01 23:25:15 +0800

    UPD: leetcode 75

--- leetcode/75/sort_colors.py
+++ leetcode/75/sort_colors.py
@@ -4,6 +4,7 @@
 # @Date  : 2020-07-18
 # @Desc  :
 from typing import List
+from copy import deepcopy
 
 
 class Solution1:
@@ -29,7 +30,7 @@ class Solution1:
 
 
 class Solution2:
-    """单指针
+    """单指针 两次遍历
     1. 迭代遍历, 把0放到数组之首；
     2. 迭代遍历, 把1放到0之后；
     时间复杂度: O(n)
@@ -51,26 +52,57 @@ class Solution2:
 
 
 class Solution3:
-    """双指针"""
+    """双指针 一次遍历
+    参考：https://leetcode-cn.com/problems/sort-colors/solution/kuai-su-pai-xu-partition-guo-cheng-she-ji-xun-huan/
+    时间复杂度: O(n)
+    空间复杂度: O(1)
+    """
     def sortColors(self, nums: List[int]) -> None:
-        pass
+        size = len(nums)
+        if size < 2:
+            return
+        i, zero, two = 0, 0, size-1
+        while i <= two:
+            if nums[i] == 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
+                i += 1
+            elif nums[i] == 1:
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[two] = nums[two], nums[i]
+                two -= 1
+
+        return nums
 
 
 def main():
     test_cases = [
-        [2, 0, 2, 1, 1, 0]
+        [2, 0, 2, 1, 1, 0],
+        [2, 0, 1],
+        [1, 2, 0]
     ]
     print('Solution1')
     s1 = Solution1()
     for test_case in test_cases:
+        test_case = deepcopy(test_case)
         s1.sortColors(test_case)
         print(test_case)
 
     print('Solution2')
     s2 = Solution2()
     for test_case in test_cases:
+        test_case = deepcopy(test_case)
         s2.sortColors(test_case)
         print(test_case)
+
+    print('Solution3')
+    s3 = Solution3()
+    for test_case in test_cases:
+        test_case = deepcopy(test_case)
+        s3.sortColors(test_case)
+        print(test_case)
+
 
 if __name__ == '__main__':
     main()
