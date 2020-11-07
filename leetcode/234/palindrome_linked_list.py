482676969e5062add34dd4744326cf1257566787
cleland <clelandgt@163.com>
2020-11-07 20:39:46 +0800

    UPD: leetcode 234

--- leetcode/234/palindrome_linked_list.py
+++ leetcode/234/palindrome_linked_list.py
@@ -30,7 +30,7 @@ class Solution1:
 
 
 class Solution2:
-    """
+    """ 快慢指针 + 反转链表
     时间复杂度: O(n)
     空间复杂度: O(n)
     """
@@ -62,6 +62,34 @@ class Solution2:
         return True
 
 
+class Solution3:
+    """ 快慢指针 + 栈
+    时间复杂度: O(n)
+    空间复杂度: O(n)
+    """
+    def isPalindrome(self, head: ListNode) -> bool:
+        if head is None or head.next is None:
+            return True
+
+        slow, fast = head, head
+        stack = [slow.val]
+        # 遍历到中间节点, slow入栈节点数据
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast.next:
+                stack.append(slow.val)
+
+        # slow后续遍历的数据等于之前入栈的数据
+        slow = slow.next
+        while slow:
+            num = stack.pop()
+            if slow.val != num:
+                return False
+            slow = slow.next
+        return True
+
+
 def main():
     test_cases = [
         ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(3, ListNode(2, ListNode(1))))))),
@@ -77,6 +105,11 @@ def main():
 
     print('Solution2')
     s = Solution2()
+    for test_case in test_cases:
+        print(s.isPalindrome(test_case))
+
+    print('Solution3')
+    s = Solution3()
     for test_case in test_cases:
         print(s.isPalindrome(test_case))
 
