01be1b3a8d29d04f5e50ab22e4bcc82b15921202
cleland <clelandgt@163.com>
2020-11-07 23:08:49 +0800

    UPD: leetcode 234

--- leetcode/234/palindrome_linked_list.py
+++ leetcode/234/palindrome_linked_list.py
@@ -7,6 +7,7 @@
 回文数: 若将n的各位数字反向排列所得自然数n1与n相等，则称n为一回文数.
 n1=1234321, n=123321 都是回文数
 """
+from copy import deepcopy
 
 
 # Definition for singly-linked list.
@@ -30,10 +31,46 @@ class Solution1:
 
 
 class Solution2:
-    """ 快慢指针 + 反转链表
+    """ 遍历链表到数组里，然后使用双指针分别从两端向中间遍历比较
     时间复杂度: O(n)
     空间复杂度: O(n)
     """
+    def isPalindrome(self, head: ListNode) -> bool:
+        if head is None or head.next is None:
+            return True
+
+        nums = []
+        p = head
+        while p:
+            nums.append(p.val)
+            p = p.next
+        left, right = 0, len(nums)-1
+        while left <= right:
+            if nums[left] != nums[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+
+class Solution3:
+    """ 快慢指针 + 反转链表
+    时间复杂度: O(n)
+    空间复杂度: O(1)
+    """
+    def reverse_link(self, head):
+        if head is None or head.next is None:
+            return head
+        pre, cur = None, head
+        while cur:
+            next = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next
+
+        return pre
+
     def isPalindrome(self, head: ListNode) -> bool:
         # 遍历到中端位置
         fast = head
@@ -43,13 +80,7 @@ class Solution2:
             slow = slow.next
 
         # 反转后半段链表
-        current_node = None
-        last_node = None
-        p = slow
-        while p:
-            current_node = ListNode(p.val, last_node)
-            last_node = current_node
-            p = p.next
+        current_node = self.reverse_link(slow)
 
         # 比较前与后半段
         p = head
@@ -62,7 +93,7 @@ class Solution2:
         return True
 
 
-class Solution3:
+class Solution4:
     """ 快慢指针 + 栈
     时间复杂度: O(n)
     空间复杂度: O(n)
@@ -95,23 +126,29 @@ def main():
         ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(3, ListNode(2, ListNode(1))))))),
         ListNode(1, ListNode(2, ListNode(3, ListNode(3, ListNode(2, ListNode(1)))))),
         ListNode(1, ListNode(2)),
+        ListNode(1, ListNode(0, ListNode(0))),
         ListNode(1),
     ]
 
     print('Solution1')
     s = Solution1()
     for test_case in test_cases:
-        print(s.isPalindrome(test_case))
+        print(s.isPalindrome(deepcopy(test_case)))
 
     print('Solution2')
     s = Solution2()
     for test_case in test_cases:
-        print(s.isPalindrome(test_case))
+        print(s.isPalindrome(deepcopy(test_case)))
 
     print('Solution3')
     s = Solution3()
     for test_case in test_cases:
-        print(s.isPalindrome(test_case))
+        print(s.isPalindrome(deepcopy(test_case)))
+
+    print('Solution4')
+    s = Solution4()
+    for test_case in test_cases:
+        print(s.isPalindrome(deepcopy(test_case)))
 
 
 if __name__ == '__main__':
