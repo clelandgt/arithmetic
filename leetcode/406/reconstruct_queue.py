445e7efef0682e3166ca68677e9d8b71a35926f1
cleland <clelandgt@163.com>
2020-11-16 22:25:50 +0800

    ADD: leetcode 406

---
+++ leetcode/406/reconstruct_queue.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @File  : reconstruct_queue.py
+# @Author: clelandgt@163.com
+# @Date  : 11/16/20
+# @Desc  :
+from typing import List
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        pass
