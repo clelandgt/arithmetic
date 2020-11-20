b594bb8c4032258849af3210a158cdb275bc5c75
cleland <clelandgt@163.com>
2020-11-20 08:44:37 +0800

    UPD: leetcode 560

--- leetcode/560/subarray_sum_equals_k.py
+++ leetcode/560/subarray_sum_equals_k.py
@@ -14,10 +14,8 @@ class Solution1:
     def subarraySum(self, nums: List[int], k: int) -> int:
         res = 0
         for i in range(len(nums)):
-            sum = nums[i]
-            if sum == k:
-                res += 1
-            for j in range(i+1, len(nums)):
+            sum = 0
+            for j in range(i, len(nums)):
                 sum += nums[j]
                 if sum == k:
                     res += 1
