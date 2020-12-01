b06a51dadaa4babb3955c3be8fe6de2bcd42e8ba
cleland <clelandgt@163.com>
2020-12-01 22:35:08 +0800

    UPD: insert sort

--- basic/python/sort/insert_sort.py
+++ basic/python/sort/insert_sort.py
@@ -6,30 +6,36 @@
 """
 已排序的[0, n-1]加入n后再排序。
 """
+from copy import deepcopy
 
 
 def insert_sort1(nums):
-    """循环"""
+    """循环
+    时间复杂度: O(n^2)
+    """
     for i in range(len(nums)):
         for j in range(1, i+1)[::-1]:
-            if nums[j] < nums[j-1]:
-                nums[j], nums[j-1] = nums[j-1], nums[j]
-
+            if nums[j-1] > nums[j]:
+                nums[j-1], nums[j] = nums[j], nums[j-1]
+            else:
+                break
     return nums
 
 
 def insert_sort2(nums):
-    """递归"""
-
+    """递归
+    时间复杂度: O(n^2)
+    """
     def _insert_sort2(nums, n):
-        if n == 0:
+        if n == len(nums):
             return
-        _insert_sort2(nums, n-1)
-        for i in range(1, n)[::-1]:
-            if nums[i] < nums[i-1]:
-                nums[i], nums[i-1] = nums[i-1], nums[i]
-    _insert_sort2(nums, len(nums))
-
+        for i in range(1, n+1)[::-1]:
+            if nums[i-1] > nums[i]:
+                nums[i-1], nums[i] = nums[i], nums[i-1]
+            else:
+                break
+        _insert_sort2(nums, n+1)
+    _insert_sort2(nums, 1)
     return nums
 
 
@@ -41,13 +47,13 @@ def main():
     print('Solution1')
     for test_case in test_cases:
         print('before sort:', test_case)
-        result = insert_sort1(test_case)
+        result = insert_sort1(deepcopy(test_case))
         print('after sort:', result)
 
-    print('Solution2')
+    print('\nSolution2')
     for test_case in test_cases:
         print('before sort:', test_case)
-        result = insert_sort2(test_case)
+        result = insert_sort2(deepcopy(test_case))
         print('after sort:', result)
 
 
